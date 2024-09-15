63ec6a9faa3ccfdd6afe647b2d2d975e3fc723ff
NitishBelagaliNEU <belagali.n@northeastern.edu>
2024-09-15 16:49:33 -0400

    logging and exception modified

--- src/exception.py
+++ src/exception.py
@@ -1,24 +1,10 @@
 import sys
-import logging
+from src.logger import logging
 import os
 from datetime import datetime
 
 # Setting up the log file path with a dynamic filename
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
-os.makedirs(logs_path, exist_ok=True)
 
-LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
-
-# Configuring logging
-logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
-)
-
-# Logging a start message
-logging.info("Logging has started")
 
 
 def error_message_detail(error, error_detail: sys):
